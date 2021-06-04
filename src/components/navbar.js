979049e5bf9eb23dc9fb930f024bb880b08aae07
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2021-06-04 12:47:09 -0600

    Terminado

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
-import './navbar.css';
+import './navbar.sass';
 
 export default class navbar extends React.Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav id="navi"className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">Portafolio</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@ export default class navbar extends React.Component {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href ="https://www.instagram.com/cotto5201/" target="_blank">Instagram <span className="sr-only"></span></a>
+              <a id="navi_link" className="nav-link" href ="https://www.instagram.com/cotto5201/" target="_blank">Instagram <span className="sr-only"></span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="https://www.facebook.com/javier.cotto.5872" target="_blank">Facebook</a>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import StartApp from "./components/startApp";
+import "bootstrap/dist/js/bootstrap";
 
 const StartComponent = () => {
   return <h1> New React APP</h1>;
